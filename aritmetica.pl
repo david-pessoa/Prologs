3b494b282083a106086542524534515bb94cadea
david-pessoa <pessoa.dvlbp@gmail.com>
2024-05-02 19:41:00 -0300

    achei um caminho

--- aritmetica.pl
+++ aritmetica.pl
@@ -2,7 +2,18 @@
 =< --> Menor ou igual a
 =:= --> Igual: avalia se dois valores são iguais
 = --> checa se os "objetos" são iguais, ou atribui valores às variáveis (unificação de termos)
+is --> aytribuição
 \= --> diferente
 \+ --> negação
 
 */
+
+soma(A, B, S) :- S is A + B.
+
+checa() :- write("Digite o valor de X: "), nl,
+           read(X), nl,
+           (
+           (X =< 100, write("O número é menor ou igual a 100")), nl;
+           (X > 100, write("O número é maior que 100")), nl
+           ).
+

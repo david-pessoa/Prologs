e29393abdb67fe71c61b9eb98b60f22b961f584e
David Pessoa <104323068+david-pessoa@users.noreply.github.com>
2024-05-06 09:51:14 -0300

    cria intercala...

--- Aula02.pl
+++ Aula02.pl
@@ -1,3 +1,4 @@
+%------------------------------ SPLIT ---------------------------------------------
 split([], [], []).
 split([A], [A], []).
 split([A, B | X], [A|As], [B|Bs]) :- split(X, As, Bs).
@@ -5,4 +6,10 @@ split([A, B | X], [A|As], [B|Bs]) :- split(X, As, Bs).
 split2(L, L1, L2) :- tam(L, Tam), T is Tam/2, N is ceiling(T), npri(N, L, L1, L2).
 npri(1, [A|As], [A], As).
 npri(N, [A|As], [A|X], Resto) :- N2 is N - 1, npri(N2, As, X, Resto).
-%----------------------------------------------------------------------------------
+
+tam([], 0).
+tam([A|As], N) :- tam(As, N2), N is N2 + 1.
+
+%------------------------------------MERGE SORT -----------------------------------
+intercala([], L, L).
+intercala(L, [], L).

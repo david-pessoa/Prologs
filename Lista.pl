1b983e9723b27642d8a4be3f46ab41661b88978f
david-pessoa <pessoa.dvlbp@gmail.com>
2024-05-13 20:29:33 -0300

    exercicios 4, 5 e 6

--- Lista.pl
+++ Lista.pl
@@ -12,4 +12,32 @@ apos(E, [E | L], L).
 apos(E, [X | L], R) :- apos(E, L, R1), R = R1.
 
 %--------------------------- Exercício 4 -----------------------
-%npri().
+npri(0, []).
+npri(N, Lista) :- N1 is N - 1, npri(N1, Lista1), append(Lista1, [N], Lista).
+
+%-------------------------- Exercício 5 -------------------------
+gera_m_mult(_, 0, []).
+gera_m_mult(N, M, Lista) :- M1 is M - 1,
+    (M mod N =:= 0, gera_m_mult(N, M1, Lista1), append(Lista1, [M], Lista); 
+    gera_m_mult(N, M1, Lista)).
+
+%-------------------------- Exercício 6 -------------------------
+conta(_, [], 0).
+conta(N, [X | Lista], F) :- N =:= X, conta(N, Lista, F2), F is F2 + 1;
+    conta(N, Lista, F).
+
+find_max([], _, MaxF, MaxN, MaxN).
+find_max([X | Lista], L, MaxF, MaxN, R) :-
+    conta(X, L, F),
+    F > MaxF,
+    find_max(Lista, L, F, X, R).
+find_max([X | Lista], L, MaxF, MaxN, R) :-
+    conta(X, L, F),
+    F =< MaxF,
+    find_max(Lista, L, MaxF, MaxN, R).
+
+main :-
+    Lista = [3, 3, 4, 3, 4, 8, 8, 8, 8, 8, 3, 3, 3],
+    find_max(Lista, Lista, 0, 0, R),
+
+%------------------------ Exercício 7 -----------------------------

cb178635aa5480dc5876852f59b4bf6638098c99
David Pessoa <104323068+david-pessoa@users.noreply.github.com>
2024-05-20 10:18:04 -0300

    Update RevisaoP2.pl

--- RevisaoP2.pl
+++ RevisaoP2.pl
@@ -5,9 +5,16 @@ t1(no(5,
               f(8)),
         no(3, f(4), f(6)))).
 
-/******************** Altura do Nó ***********************
+/******************** Altura do Nó ***********************/
 h(f(_), 1).
 h(no(_, Te, Td), H) :- h(Te, He), h(Td, Hd), maior(He, Hd, Hvenc), H is 1 + Hvenc.
 
 maior(A, B, A) :- A > B.
 maior(A, B, B) :- B >= A.
+
+/******************** Troca valor por altura **************/
+trocaH(f(_), f(1)).
+trocaH(no(_, Te, Td), no(H, Te2, Td2)) :-
+        h(no(_, Te, Td), H),
+        trocaH(Te, Te2),
+        trocaH(Td, Td2).

4dc6a099d10b2ffaf0164c3d8a7ae877341333f9
David Pessoa <104323068+david-pessoa@users.noreply.github.com>
2024-05-20 09:56:44 -0300

    Create RevisaoP2.pl

---
+++ RevisaoP2.pl
@@ -0,0 +1,13 @@
+% Arvore T1
+t1(no(5,
+        no(7, 
+              no(2, f(1), f(9)),
+              f(8)),
+        no(3, f(4), f(6)))).
+
+/******************** Altura do Nó ***********************
+h(f(_), 1).
+h(no(_, Te, Td), H) :- h(Te, He), h(Td, Hd), maior(He, Hd, Hvenc), H is 1 + Hvenc.
+
+maior(A, B, A) :- A > B.
+maior(A, B, B) :- B >= A.

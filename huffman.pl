81672ca27869719f67bdaf376808985889a093d3
david-pessoa <pessoa.dvlbp@gmail.com>
2024-05-21 17:35:09 -0300

    add comments

--- huffman.pl
+++ huffman.pl
@@ -1,32 +1,35 @@
+% David Pessoa 10402647
+
 :- use_module(library(pairs)).
 :- use_module(library(pio)).
 
 huffman :-
     %ler mensagem do in.txt
-    read_file_to_string('in.txt', L, []),
-    atom_chars(L, LA),
-    msort(LA, LS),
-    packList(LS, PL),
-    sort(PL, PLS),
-    build_tree(PLS, A),
-    coding(A, [], C),
-    sort(C, SC),
+    read_file_to_string('in.txt', L, []), %lê arquivo
+    atom_chars(L, LA), %transforma a string L numa lista contendo os caracteres da string L
+    msort(LA, LS), %Ordena lista LA
+    packList(LS, PL), %Cria lista com as frequências de cada caractere
+    sort(PL, PLS), %Ordena a lista, colocando os caracteres de menor sequência na frente
+    build_tree(PLS, A), %Cria a árvore de huffman A
+    coding(A, [], C), %Gera tabela C com a relação entre número de frequência, caractere e número binário correspondentes.
+    sort(C, SC), %Ordena C
 
     % Substituir caracteres pelos códigos binários correspondentes
     substituir_caracteres(LA, SC, ListaMensagemCodificada),
 
-    concatenar_lista(ListaMensagemCodificada, MensagemCodificada),
+    concatenar_lista(ListaMensagemCodificada, MensagemCodificada), %Apartir da lista com os caracteres codificados, gera-se a string MensagemCodificada
     escrever_em_arquivo(MensagemCodificada, 'out.txt'),
 
-    atom_chars(MensagemCodificada, ListaBits),
-    decodificar(ListaBits, A, A, []).
+    atom_chars(MensagemCodificada, ListaBits), %Transforma a MensagemCodificada numa lista de bits
+    decodificar(ListaBits, A, A, []). %decodifica a mensagem (resultado impresso dentro da função, na última recursão)
 
-
+%Constrói árvore
 build_tree([[V1|R1], [V2|R2]|T], AF) :- 
     V is V1 + V2, 
     A = [V, [V1|R1], [V2|R2]],
     (   T=[] -> AF=A ;  sort([A|T], NT), build_tree(NT, AF) ).
 
+%Gera tabela com a relação entre número de frequência, caractere e número binário
 coding([_A,FG,FD], Code, CF) :-
     (   is_node(FG) ->  coding(FG, [0 | Code], C1)
              ;  leaf_coding(FG, [0 | Code], C1) ),
@@ -40,11 +43,7 @@ leaf_coding([FG,FD], Code, CF) :-
 
 is_node([_V, _FG, _FD]).
 
-print_code([N, Car, Code]):-
-    format('~w :~t~w~t~30|', [Car, N]),
-    forall(member(V, Code), write(V)),
-    nl.
-
+%Cria lista com as frequências de cada caractere
 packList([], []).
 packList([X], [[1,X]]) :- !.
 packList([X|Rest], [XRun|Packed]):-
@@ -58,6 +57,7 @@ run(V, [V|LRest], [N1,V], RRest):-
 run(V, [Other|RRest], [1,V], [Other|RRest]):-
     dif(V, Other).
 
+% Substituir caracteres pelos códigos binários correspondentes
 substituir_caracteres([], _, []).
 substituir_caracteres([Caractere|MensagemRestante], Codificacao, MensagemCodificadaRestante) :-
     (   member([_, Caractere, CodigoBinario | X], Codificacao) ->
@@ -66,18 +66,19 @@ substituir_caracteres([Caractere|MensagemRestante], Codificacao, MensagemCodific
     ),
     substituir_caracteres(MensagemRestante, Codificacao, MensagemCodificadaRestanteRestante).
 
+%Transforma uma lista em string
 concatenar_lista([], '').
 concatenar_lista([String|Resto], StringConcatenada) :-
     concatenar_lista(Resto, RestoConcatenado),
     string_concat(String, RestoConcatenado, StringConcatenada).
 
-
+%Escreve no arquivo "out.txt"
 escrever_em_arquivo(String, NomeArquivo) :-
     open(NomeArquivo, write, Stream, [encoding(utf8)]),
     write(Stream, String),
     close(Stream).
 
-
+%Transforma a uma lista de inteiros para uma string
 lista_inteiros_para_string([], '').
 lista_inteiros_para_string([Inteiro|Resto], String) :-
     convert(Caractere, Inteiro),
@@ -85,10 +86,12 @@ lista_inteiros_para_string([Inteiro|Resto], String) :-
     string_chars(RestoString, RestoChars),
     string_chars(String, [Caractere|RestoChars]).
 
+%Converte o bit de inteiro para string
 convert('0', 0).
 convert('1', 1).
 
-decodificar([], [_, Char], _, Decodifica) :- 
+%Decodifica a mensagem percorrendo a árvore
+decodificar([], [_, Char], _, Decodifica) :-
     append(Decodifica, [Char], Deci), 
 	concatenar_lista(Deci, MensagemDecodificada),
 	writeln("Mensagem decodificada:"),

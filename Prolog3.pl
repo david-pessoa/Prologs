eddee83e0a44d66a86a0133b39cce8b1da308d2a
david-pessoa <pessoa.dvlbp@gmail.com>
2024-05-26 14:27:32 -0300

    finaliza exercícios da aula 3 do moodle

--- Prolog3.pl
+++ Prolog3.pl
@@ -35,5 +35,37 @@ totn(N, I, Sum, [X| Lista], ListaRes) :-
 totn(N, Lista, Resultado) :-
     totn(N, 1, 0, Lista, Resultado).
 
-%------------------------ Mantém elementos de N em N e elina o resto ---------------------
-    
+%------------------------ Mantém elementos de N em N e elimina o resto ---------------------
+
+mantem(_, _, [], []).
+
+mantem(N, I, [X | Lista], [X | ListaRes]) :-
+    0 is mod(I, N), I2 is I + 1, 
+    mantem(N, I2, Lista, ListaRes).
+
+mantem(N, I, [_ | Lista], ListaRes) :-
+    Y is mod(I, N), I2 is I + 1, Y \= 0,
+    mantem(N, I2, Lista, ListaRes).
+
+%---------------------------- enúplica os elementos de uma lista -----------------------
+multiplica(0, _, []).
+multiplica(N, E, [E | R]) :- N > 0, N2 is N - 1, multiplica(N2, E, R).
+
+enuplica(_, [], []).
+enuplica(N, [X | Lista], ListaRes) :-
+    multiplica(N, X, L2),
+    enuplica(N, Lista, Lista2), append(L2, Lista2, ListaRes).
+
+%------- Responde verdadeiro se a soma dos maiores ou iguais a N é maior que a soma dos menores que N---------
+
+verifica(N, L) :- verifica(N, 0, 0, L).
+
+verifica(_, SumMenores, SumMaiores, []) :- SumMaiores > SumMenores.
+
+verifica(Num, SumMenores, SumMaiores, [X | Lista]) :-
+    X >= Num, SumMaiores2 is SumMaiores + X,
+    verifica(Num, SumMenores, SumMaiores2, Lista).
+
+verifica(Num, SumMenores, SumMaiores, [X | Lista]) :-
+    X < Num, SumMenores2 is SumMenores + X,
+    verifica(Num, SumMenores2, SumMaiores, Lista).
